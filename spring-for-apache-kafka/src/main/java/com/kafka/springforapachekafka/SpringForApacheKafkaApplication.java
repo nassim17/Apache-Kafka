8e83cd6143bc23f8a208831361a05950451e4648
nabed <abednassim92@gmail.com>
2024-01-04 14:01:43 +0100

    Spring-for-apache-kafka: Init Project.

---
+++ spring-for-apache-kafka/src/main/java/com/kafka/springforapachekafka/SpringForApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.springforapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ spring-for-apache-kafka/src/test/java/com/kafka/springforapachekafka/SpringForApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.springforapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
