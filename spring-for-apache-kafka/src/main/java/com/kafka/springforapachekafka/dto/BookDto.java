153756f4940fa4fc737ae210ad2a2eb8c959064c
nabed <abednassim92@gmail.com>
2024-01-04 14:38:17 +0100

    Spring-for-apache-kafka: Create BookDto.

---
+++ spring-for-apache-kafka/src/main/java/com/kafka/springforapachekafka/dto/BookDto.java
@@ -0,0 +1,19 @@
+package com.kafka.springforapachekafka.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+
+  private Long id;
+  private String title;
+  private String author;
+  private int publicationYear;
+}
